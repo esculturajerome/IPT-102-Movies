6ee28be3e2563ac766112427a521d77132a3c920
Jerome Escultura <esculturajerome@gmail.com>
2023-10-07 08:05:33 +0800

    initial setup

---
+++ BSIT3L-Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BSIT3L_Movies.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

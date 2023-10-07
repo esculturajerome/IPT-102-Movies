ac637e2d62924e0143158458a10b834d896a707b
Jerome Escultura <esculturajerome@gmail.com>
2023-10-07 09:50:35 +0800

    add movies controller,model and view

---
+++ BSIT3L-Movies/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BSIT3L_Movies.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSIT3L_Movies.Controllers
+{
+	public class MoviesController : Controller
+    {
+        private List<Movie> _movies;
+        public MoviesController()
+		{
+            // Sample movie data
+            _movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Titanic", Rating = "5" },
+                new Movie { Id = 2, Name = "Inception", Rating = "4" },
+                new Movie { Id = 3, Name = "The Shawshank Redemption", Rating = "5" }
+            };
+        }
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Avatar", Rating = "5" };
+            return View(movie);
+        }
+    }
+}
+

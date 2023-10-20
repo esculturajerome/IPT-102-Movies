7ee21b69f408ed67f83517cc3e4aa45054a58c42
raymondramos <146494262+raymondramos@users.noreply.github.com>
2023-10-20 15:39:17 +0800

    Add files via upload

---
+++ MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BSIT3L_Movies.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSIT3L_Movies.Controllers
+{
+	public class MoviesController : Controller
+    {
+        private List<MovieViewModel> _movies;
+        public MoviesController()
+		{
+            // Sample movie data
+            _movies = new List<MovieViewModel>
+            {
+               new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
+            new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
+            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+            };
+        }
+        public ActionResult Random()
+        {
+            var movie = new MovieViewModel() { Name = "Avatar", Rating = "5" };
+            return View(movie);
+        }
+        public ActionResult GetMovie(int id)
+        {
+            var movie = _movies.Find(m => m.Id == id);
+            if (movie == null)
+                return NotFound();
+            return View(movie);
+
+        }
+
+    }
+}
+
---
+++ MoviesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BSIT3L_Movies.Models
+{
+    
+	public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Rating { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        
+    }
+}
+

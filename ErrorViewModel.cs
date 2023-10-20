3e9f9a6eba89356b36a0d6c1f43a767c7283a119
raymondramos <146494262+raymondramos@users.noreply.github.com>
2023-10-20 15:43:26 +0800

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BSIT3L_Movies.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-

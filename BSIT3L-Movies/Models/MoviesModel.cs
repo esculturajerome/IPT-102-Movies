6937dba4c29b97e2d0b05baab8f63a88597dce13
raymondramos <146494262+raymondramos@users.noreply.github.com>
2023-10-20 15:46:18 +0800

    Dynamic Ways

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -1,15 +1,17 @@
-﻿using System;
+using System;
 namespace BSIT3L_Movies.Models
 {
-    
-	public class MovieViewModel
+
+    public class MovieViewModel
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Rating { get; set; }
-        public int ReleaseYear { get; set; }
-        public string Genre { get; set; }
-        
+        public string Image { get; set; }
+        public string Teaser { get; set; }
     }
+
 }
+
 
